d5e66b17506529593274c57cbc93160c075cb76b
4ge44koyama <m220552a@gmail.com>
2020-04-27 00:02:23 +0900

    add_user-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,30 @@
 class UsersController < ApplicationController
 
-  before_action :set_user
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :user_params, only: :update
 
   def show
-    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @find_post = Post.find_by(user_id: current_user.id)
   end
 
-  def edit
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: 'プロフィールの編集が完了しました'
+    else
+      render :edit, notice: 'プロフィールの編集ができません'
+    end
   end
 
   private
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :image)
   end
 
 end
