9acbeab59101fedcdde56b3c49a84a3ed5cf9929
4ge44koyama <m220552a@gmail.com>
2020-04-27 18:25:48 +0900

    add_pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(6)
   end
 
   def new
