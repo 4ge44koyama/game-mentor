0000a38a7608a445ce651b8df3b3791033b6b951
4ge44koyama <m220552a@gmail.com>
2020-05-03 11:50:21 +0900

    drop_requests

--- app/assets/javascripts/request_form.js
+++
@@ -1,7 +0,0 @@
-$(document).on('turbolinks:load', function(){
-
-  $('.edit-and-destroy-links__link').on('click'){
-
-  };
-
-});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_02_063405) do
+ActiveRecord::Schema.define(version: 2020_05_03_024852) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 2020_05_02_063405) do
     t.datetime "updated_at", null: false
     t.string "fee"
     t.integer "user_id"
-  end
-
-  create_table "requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "text"
-    t.integer "user_id", null: false
-    t.integer "to_id", null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
