ea8ab74ac3b5b87c2939c46ea79711449de3a162
4ge44koyama <m220552a@gmail.com>
2020-05-20 15:32:44 +0900

    feature_test

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'post', type: :feature do
+  let(:user) { create(:user) }
+
+  scenario 'ユーザー情報が更新されていること' do
+    # ログイン前には投稿をするボタンがない
+    visit root_path
+    expect(page).to have_no_content('投稿をする')
+
+    # ログインする
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    expect(page).to have_content('投稿をする')
+
+    # メンター記事を投稿する
+    expect {
+      click_link('投稿をする')
+      expect(current_path).to eq new_post_path
+      fill_in 'post_title', with: 'フィーチャスペックのテスト'
+      fill_in 'post_content', with: 'フィーチャスペックのテスト'
+      fill_in 'post_fee', with: '1000円 / 月'
+      find('input[type="submit"]').click
+    }.to change(Post, :count).by(1)
+  end
+end
